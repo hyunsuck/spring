143ad8fdaa255f09d900b4fd4e49b86fa9c4b031
Hyun suck <dhm986911@naver.com>
2025-07-03 17:53:30 +0900

    java commit

--- sp02/src/main/java/com/yedam/app/common/web/ParameterController.java
+++ sp02/src/main/java/com/yedam/app/common/web/ParameterController.java
@@ -1,8 +1,14 @@
 package com.yedam.app.common.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.app.emp.service.EmpVO;
@@ -24,5 +30,47 @@ public class ParameterController {
 		result += "\t last_name : " + empVO.getLastName();
 		return result;
 	}
+		
+	// QueryString, 2) @RequestParam (기본타입, 단일값)
+	@RequestMapping("reqparm")
+	@ResponseBody
+	public String requestParam
+	(@RequestParam Integer employeeId,  // 필수
+					String lastName) {   //생략가능
+		String result = "";
+		result += "Path : /reqparm \n";
+		result += "\t employee_id : " + employeeId;
+		result += "\t last_name : " + lastName;
+		return result;
+	}
 	
+	// @PathVariable : 경로에 값을 포함하는 방식, 단일 값
+	// Method는 상관없음
+	// Content-type도 상관없음
+	@RequestMapping("path/{id}") // path/Hong, path/1234
+	@ResponseBody
+	public String pathVariable(@PathVariable String id) {
+		String result = "";  
+		result += "Path : /path/{id} \n";
+		result += "\t id : " + id;
+		return result;
+	}
+	
+	
+	//@RequestBody : JSON 포맷, 배열 or 객체
+	// Method : POST, PUT
+	// Content-type : application/json
+	@PostMapping("resbody")
+	@ResponseBody
+	public EmpVO requestBody(@RequestBody EmpVO empVO) {
+		return empVO;
+	}
+	
+	
+	@PostMapping("resbodyList")
+	@ResponseBody
+	public List<EmpVO> resbodyList
+				(@RequestBody List<EmpVO> list) {
+		return list;		
+	}
 }
